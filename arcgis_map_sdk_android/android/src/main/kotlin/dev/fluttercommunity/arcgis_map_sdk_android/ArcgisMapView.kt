03aacd27aa5f00de7794543aad6db5e26ece7005
Julian Bissekkou <36447137+JulianBissekkou@users.noreply.github.com>
2026-03-25 11:02:30 +0100

    feature/improve-error-message-dispose (#110)
    * update logging
    
    * handle dispose method call earlier

--- arcgis_map_sdk_android/android/src/main/kotlin/dev/fluttercommunity/arcgis_map_sdk_android/ArcgisMapView.kt
+++ arcgis_map_sdk_android/android/src/main/kotlin/dev/fluttercommunity/arcgis_map_sdk_android/ArcgisMapView.kt
@@ -168,8 +168,14 @@ internal class ArcgisMapView(
 
     private fun setupMethodChannel() {
         methodChannel.setMethodCallHandler { call, result ->
+            if (call.method == "dispose") {
+                // Only used on iOS for now. No need to cleanup anything on android
+                result.success(true)
+                return@setMethodCallHandler
+            }
+
             if (isDisposed) {
-                result.error("disposed", "ArcgisMapView has been disposed", null)
+                result.error("disposed", "Map called with ${call.method} but ArcgisMapView has been disposed", null)
                 return@setMethodCallHandler
             }
             when (call.method) {
@@ -220,8 +226,6 @@ internal class ArcgisMapView(
 
                 "export_image" -> onExportImage(result)
 
-                "dispose" -> onDispose(result)
-
                 else -> result.notImplemented()
             }
         }
@@ -257,11 +261,6 @@ internal class ArcgisMapView(
                 result.finishWithError(e)
             }
         }
-    }
-
-    private fun onDispose(result: MethodChannel.Result) {
-        // Only used on iOS for now. No need to cleanup anything on android
-        result.success(true)
     }
 
     private fun onSetLocationDisplayDefaultSymbol(call: MethodCall, result: MethodChannel.Result) {
